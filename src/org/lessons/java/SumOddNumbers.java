ff45f791d0a821d1066b3308d56a18cb83c39f9e
francescotrudu <francescotrudu84@gmail.com>
2023-05-05 14:42:08 +0200

    Fix

--- src/org/lessons/java/SumOddNumbers.java
+++ src/org/lessons/java/SumOddNumbers.java
@@ -3,14 +3,14 @@ package org.lessons.java;
 public class SumOddNumbers {
 
 	public static void main(String[] args) {
-		int[] intNumbers = {1, 4, 33, 12, 5, 9, 11, 1};
+		int[] intNumbers = {1, 4, 33, 12, 5, 9, 11, 1, 2};
 		
 		int sum = 0;
 		
-		for(int i = 0; i < intNumbers.length; i+=2)
-			if (i % 2 != 0) {
-				sum += intNumbers[i]; 
-			}
+		for(int i = 0; i < intNumbers.length; i+=2) {
+			int val = intNumbers[i];
+			sum += val;
+		}
 		System.out.println(sum);
 	}
 
