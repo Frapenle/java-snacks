9aaacca2c7794b0f1a6fdac92a8680ce9366713c
francescotrudu <francescotrudu84@gmail.com>
2023-05-05 11:34:39 +0200

    add name and surname snack

---
+++ src/org/lessons/java/NameSurname.java
@@ -0,0 +1,25 @@
+package org.lessons.java;
+
+import java.util.Random;
+
+public class NameSurname {
+
+	public static void main(String[] args) {
+		String[] name = {"Francesco", "Marta", "Claudia", "Giovanni"};
+		String[] surname = {"Rossi", "Bianchi", "Totti", "Dionisi"};
+		Random randomAlias = new Random();
+		for (int i=0; i < name.length; i++) {
+			int rndName = randomAlias.nextInt(name.length);
+			int rndSurname = randomAlias.nextInt(surname.length);
+			
+			String rName = name[rndName];
+            String rSurname = surname[rndSurname];
+            
+            System.out.println(rName + " " + rSurname);
+			
+		}
+		
+
+	}
+
+}
