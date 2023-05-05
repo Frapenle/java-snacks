e442401f594c3cfdaa5bde757c5af05fd024344b
francescotrudu <francescotrudu84@gmail.com>
2023-05-05 12:39:09 +0200

    Add palindrome checker

---
+++ src/org/lessons/java/Palindroma.java
@@ -0,0 +1,23 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Palindroma {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Inserisci una parola per verificare se e' palindroma");
+		String word = sc.nextLine();
+		sc.close();
+		String reverseWord = "";
+		for (int i = word.length() - 1; i >= 0; i-- ) {
+			reverseWord += word.charAt(i);
+		}
+		
+		if(reverseWord.equals(word)) {
+			System.out.println(word + " e' palindroma");
+		} else {
+			System.out.println(word + " non e' palindroma");
+		}
+	}
+}
